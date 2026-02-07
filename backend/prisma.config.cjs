fafdd81fafb5ba3ee533ea1a4ff1dc6dbfcb0209
Koray Topuz <krytopuz@gmail.com>
2026-02-07 18:48:59 +0300

    .

--- backend/prisma.config.cjs
+++ backend/prisma.config.cjs
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "npx ts-node-dev --transpile-only prisma/seed.ts",
+    seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
     url: env("DATABASE_URL"),
